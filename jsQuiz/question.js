abba06b2e98bf5a43611b6671d0283c1a51df154
Timmy Chiu <timmyc@andrew.cmu.edu>
2019-10-02 11:29:36 -0400

    question update

--- jsQuiz/question.js
+++ jsQuiz/question.js
@@ -5,3 +5,80 @@ class Question {
     this.answer = answer;
 }
 }
+
+var questions = [
+new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
+new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+new Question("What has not been covered in PUI lab?", ["HTML", "CSS","Javascript", "Java"], "Java"),
+];
+
+new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML")
+
+class Quiz {
+    constructor(questions) {
+      this.score = 0;
+  this.questions = questions;
+      this.questionIndex = 0;
+}
+}
+
+class Quiz {
+    constructor(questions) {
+      this.score = 0;
+  this.questions = questions;
+      this.questionIndex = 0;
+    }
+
+
+    getQuestion() {
+
+    return this.questions[this.questionIndex];
+
+    }
+
+}
+
+  isCorrectAnswer(choice){
+         return this.answer === choice;
+        }
+
+  guess(answer) {
+    if(this.getQuestion().isCorrectAnswer(answer)) {
+     this.score++;
+        }
+
+    this.questionIndex++;
+}
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+     quiz.guess(guess);
+     populate();
+    }
+};
+
+
+function populate() {
+
+     var element = document.getElementById("question");
+     element.innerHTML = quiz.getQuestion().text;
+
+     // show options
+     var choices = quiz.getQuestion().choices;
+     for(var i = 0; i < choices.length; i++) {
+       var element = document.getElementById("choice" + i);
+       element.innerHTML = choices[i];
+       guess("btn" + i, choices[i]);
+     }
+
+
+};
+
+for (var i = 0; i <10; i++){
+  console.log(i);
+}
+
+
+
+
