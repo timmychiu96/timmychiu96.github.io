4e6e384fa8ec7f3d202d593563c837ccfc3a5283
Timmy Chiu <timmyc@andrew.cmu.edu>
2019-10-20 21:26:54 -0400

    added functionality, responds to user selecting flavor and quantiy. Updates price accordingly. Added shopping cart page with dropdowns.

--- homework_6/main.js
+++ homework_6/main.js
@@ -1,3 +1,61 @@
 function addItem() {
-    alert('Congrats your item was added!');
+    alert('Congratulations your item was added!');
+}
+
+var orderCounter = 0;
+var bunOne = new bun("Original", null, 1);
+
+
+//do these things after the page loads
+window.addEventListener("load", function(){
+
+    priceCalc();
+
+//referenced https://www.w3schools.com/howto/howto_js_active_element.asp
+//active state for glazing
+var glazeContainer = document.getElementById("gridglaze");
+var glazeBtns = glazeContainer.getElementsByClassName("boxglaze");
+
+for (var i = 0; i < glazeBtns.length; i++) {
+  glazeBtns[i].addEventListener("click", function() {
+    var current = glazeContainer.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+//active state for quantity
+var quantContainer = document.getElementById("gridquant");
+var quantBtns = quantContainer.getElementsByClassName("boxquant");
+
+for (var i = 0; i < quantBtns.length; i++) {
+  quantBtns[i].addEventListener("click", function() {
+    var current = quantContainer.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+});
+
+
+//constructor for the bun
+function bun(flavor, glazing, quantity) {
+    this.flavor = flavor;
+    this.glazing = glazing;
+    this.quantity = quantity;
+    this.price = 3;
+}
+
+function setQuant(quantity){
+    bunOne.quantity = quantity;
+    priceCalc();
+}
+
+function priceCalc(){
+    document.getElementById("price").textContent = "$" + bunOne.price + ".00 * " + bunOne.quantity + " = " + "$" + bunOne.price*bunOne.quantity + ".00";
+}
+
+function orderCount(){
+    orderCounter++;
+    document.getElementById("cart").textContent = "Cart (0)";
 }
